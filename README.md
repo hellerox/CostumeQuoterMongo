736d36e45a91b2de91ea61735efc7ac3dee9f5ba
Carlos <hellerox@gmail.com>
2018-07-04 20:10:42 -0500

    Actualización de Conexión y Readme
    Actualización de Conexión y Readme

--- README.md
+++ README.md
@@ -2,3 +2,49 @@
 
 API for my test application for Artist City
 
+## Install requirements
+
+dep ensure
+
+## Run
+
+go run main.go
+
+## API
+
+### **getMaterials**
+
+Returns all Materials from collection Materials
+
+- **URL**
+
+  /material
+
+- **Method:**
+
+  `GET`
+
+### **createMaterial**
+
+Create new material based on JSON request
+
+- **URL**
+
+  /material
+
+- **Method:**
+
+  `POST`
+
+### **getMaterialByObjectId**
+
+Returns one Material by ObjectId
+
+- **URL**
+
+  /material/<id>
+
+- **Method:**
+
+  `GET`
+
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
+	"net"
 	"net/http"
 
 	controllers "github.com/CostumeQuoterMongo/controller"
@@ -18,10 +21,27 @@ func main() {
 }
 
 func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost")
+
+	tlsConfig := &tls.Config{}
+
+	dialInfo := &mgo.DialInfo{
+		Addrs: []string{"cluster0-shard-00-00-qkumg.gcp.mongodb.net:27017",
+			"cluster0-shard-00-01-qkumg.gcp.mongodb.net:27017",
+			"cluster0-shard-00-02-qkumg.gcp.mongodb.net:27017"},
+		Database: "admin",
+		Username: "carlos",
+		Password: "",
+	}
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+	s, err := mgo.DialWithInfo(dialInfo)
 
 	if err != nil {
 		panic(err)
+	} else {
+		fmt.Println("Connected to DB")
 	}
 	return s
 }
